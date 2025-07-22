2d0f8e1aae394fd06585ebb65726c996af9e3005
ali4634 <muhammadali46@gmail.com>
2025-07-22 13:49:53 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+ const CACHE_NAME = 'exam-app-cache-v1';
+const REPO_NAME = '/exam/';
+
+const urlsToCache = [
+  `${REPO_NAME}`,
+  `${REPO_NAME}index.html`, // فرض کریں کہ آپ کی فائل کا نام index.html ہے
+  `${REPO_NAME}manifest.json`,
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.autotable.min.js',
+  'https://cdn.jsdelivr.net/gh/googlefonts/noto-fonts@main/unhinted/ttf/NotoNastaliqUrdu/NotoNastaliqUrdu-Regular.ttf'
+];
+
+// انسٹال ایونٹ
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// ایکٹیویٹ ایونٹ
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cache => cache !== CACHE_NAME)
+        .map(cache => caches.delete(cache))
+      );
+    })
+  );
+});
+
+// فیچ ایونٹ
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
